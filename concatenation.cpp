505902fc25e36fb93ac20ad208b4b60fd89a2caf
Jiya P <jiyapalod@gmail.com>
2024-09-05 10:59:07 +0530

    Create concatenation.cpp

---
+++ concatenation.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string first_name,last_name;
+    first_name = "Jiya";
+    last_name = "Palod";
+
+    cout<<first_name+last_name;
+}
+
+/*
+OUTPUT: 
+JiyaPalod
+*/
